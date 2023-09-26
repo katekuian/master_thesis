4a1905e898776b44a0b3a2b8012a29a18a0aa01d
Kate Kuian <kate.kuian@gmail.com>
2023-09-26 10:09:34 +0200

    Outsource batch size and checkpoint to config

--- src/config.py
+++ src/config.py
@@ -1,22 +1,34 @@
 # crop = 'broad_bean'
-# crop = 'common_buckwheat'
+crop = 'common_buckwheat'
 # crop = 'pea'
 # crop = 'corn'
 # crop = 'soybean'
 # crop = 'sunflower'
-crop = 'sugar_beet'
+# crop = 'sugar_beet'
 
 # model_type = 'multiclass'
 model_type = 'binary'
 
 crops = [ 
-    'broad_bean'
-    # 'common_buckwheat',
-    # 'pea',
-    # 'corn',
-    # 'soybean',
-    # 'sunflower',
-    # 'sugar_beet'
+    # 'broad_bean'
+    'common_buckwheat',
+    'pea',
+    'corn',
+    'soybean',
+    'sunflower',
+    'sugar_beet'
 ]
 
 model_types = ['binary', 'multiclass']
+
+
+# checkpoint = "nvidia/mit-b0"
+# checkpoint = "facebook/maskformer-swin-large-coco"
+# checkpoint = "microsoft/beit-base-finetuned-ade-640-640"
+# checkpoint = "Intel/dpt-large-ade"
+# checkpoint = "apple/deeplabv3-mobilevit-xx-small"
+checkpoint = "openmmlab/upernet-swin-tiny"
+
+
+# batch_size = 6
+batch_size = 2
--- src/model_training.py
+++ src/model_training.py
@@ -9,17 +9,17 @@ import codecs
 import os
 from os import sys
 
-from transformers import SegformerImageProcessor, AutoModelForSemanticSegmentation, TrainingArguments, Trainer, EarlyStoppingCallback
+from transformers import AutoImageProcessor, AutoModelForSemanticSegmentation, TrainingArguments, Trainer, EarlyStoppingCallback
 module_path = os.path.abspath(os.path.join('./src'))
 if module_path not in sys.path:
     sys.path.append(module_path)
 from data_prepossessing import create_datasets_for_plants, get_labels
 from constants import seed, weed_plants, models_folder
-from config import model_type, crop, model_types, crops
+from config import model_type, crop, model_types, crops, checkpoint, batch_size
 
 
 def init_image_processor(checkpoint):
-    image_processor = SegformerImageProcessor.from_pretrained(checkpoint)
+    image_processor = AutoImageProcessor.from_pretrained(checkpoint)
     return image_processor
 
 
@@ -60,8 +60,8 @@ def init_training_arguments(prediction_loss_only):
         output_dir="segformer-b0-scene-parse-150",
         learning_rate=6e-5,
         num_train_epochs=100,
-        per_device_train_batch_size=6,
-        per_device_eval_batch_size=6,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
         save_total_limit=3,
         evaluation_strategy="steps",
         save_strategy="steps",
@@ -90,14 +90,11 @@ def initialize_trainer(model, training_args, num_labels, metric, train_ds, test_
         train_dataset=train_ds,
         eval_dataset=test_ds,
         compute_metrics=lambda eval_pred: compute_metrics(num_labels, metric, eval_pred),
-        callbacks=[EarlyStoppingCallback(early_stopping_patience=3)]
+        callbacks=[EarlyStoppingCallback(early_stopping_patience=5)]
     )
 
 
 def train_model_of_type_for_crop(model_type, crop):
-    # Define a model checkpoint to be finetuned
-    checkpoint = "nvidia/mit-b0"
-
     # Prepare the data for the model training
     model_plant_names = [crop] + weed_plants
     train_ds, test_ds = create_datasets_for_plants(model_plant_names, model_type, crop)
@@ -118,7 +115,7 @@ def train_model_of_type_for_crop(model_type, crop):
     print('label2id:', label2id)
 
     # Initialize and train model
-    model = AutoModelForSemanticSegmentation.from_pretrained(checkpoint, id2label=id2label, label2id=label2id)
+    model = AutoModelForSemanticSegmentation.from_pretrained(checkpoint, id2label=id2label, label2id=label2id, ignore_mismatched_sizes=True)
     training_args_for_training = init_training_arguments_for_training()
     metric = evaluate.load("mean_iou")
     trainer = initialize_trainer(model, training_args_for_training, num_classses, metric, train_ds, test_ds)
@@ -151,4 +148,7 @@ def train_all_models_from_config():
 
 
 if __name__ == '__main__':
-    print(train_all_models_from_config())
+    # print(train_all_models_from_config())
+    print(train_model_from_config())
+
+    
